3501574cf90366097e2ae7d2f14963ddbdfe066b
Msuf123 <akshatmalik18t@gmail.com>
2024-06-11 04:22:59 +0530

    Learnt about untracked and how to cleanup a effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
-import { Component, Injector, computed, effect, signal } from '@angular/core';
+import { Component, Injector, computed, effect, signal, untracked } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+      
+import _ from 'lodash';
+import { single } from 'rxjs';
+const data = signal(['test'], {equal: _.isEqual});
+// Even though this is a different array instance, the deep equality
+// function will consider the values to be equal, and the signal won't
+// trigger any updates.
+data.set(['test']);
 
+    
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,24 +20,25 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'signals';
   counter=signal(1)
+  counter_two=signal(2)
   showCounter=signal(false)
   doubleCounter=computed(()=>{
     
-    return this.showCounter()?this.counter()*2:'None'
+    return this.showCounter()?untracked(this.counter)*2:'None'
   
   })
   constructor(private injector: Injector){
     this.counter.set(3)
     console.log(this.doubleCounter())
-    this.counter.set(2)
+    this.counter.set(9)
     this.showCounter.set(true)
     console.log(this.doubleCounter())
-    const a=effect(()=>{console.log('I changed'+this.counter())})       
-    a.destroy()
+    const a=effect(()=>{console.log('I changed'+untracked(this.counter),this.doubleCounter())})       
+  
   }
   customEffect(){
-    this.counter.set(9)
-    effect(()=>{console.log('I changed'+this.counter())},{injector:this.injector}) 
+    this.counter.update((a)=>++a)
+    //effect(()=>{console.log('I changed'+this.counter())},{injector:this.injector}) 
   }
   
 }
