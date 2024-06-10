d21b3e84383dba4e479a50243a232d548147864a
Msuf123 <akshatmalik18t@gmail.com>
2024-06-10 23:34:57 +0530

    Done with the effect in signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, Injector, computed, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -17,13 +17,18 @@ export class AppComponent {
     return this.showCounter()?this.counter()*2:'None'
   
   })
-  constructor(){
+  constructor(private injector: Injector){
     this.counter.set(3)
     console.log(this.doubleCounter())
     this.counter.set(2)
     this.showCounter.set(true)
     console.log(this.doubleCounter())
-   effect(()=>{console.log('I changed')})       
+    const a=effect(()=>{console.log('I changed'+this.counter())})       
+    a.destroy()
+  }
+  customEffect(){
+    this.counter.set(9)
+    effect(()=>{console.log('I changed'+this.counter())},{injector:this.injector}) 
   }
   
 }
